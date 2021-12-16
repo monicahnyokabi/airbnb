dd35bf772fe619807c82e7121e11814ccf6e333f
monicahnyokabi <wambuinyokabimn@gmail.com>
2021-12-16 21:44:34 +0300

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>@2021 (Jan Challenge) Monicah Wambui </p>
+            <p>Happy hacking!!</p>
+        </div>
+    )
+}
+
+export default Footer;  
